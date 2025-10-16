94aec81130eeef4357e188516e4a7a990aadf6f0
Nourin <nourinza.ju@gmail.com>
2025-10-16 02:53:29 -0500

    Added code comments - fixes #1

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
-print("Hello, World!")
-print("Hi! Conflict resolved successfully")
+# This is a simple Hello World program
+# Created for SE Assignment 2
+
+print("Hello, World!")  # Display greeting
+print("Hi! Conflict resolved successfully")  # Display personalized message
